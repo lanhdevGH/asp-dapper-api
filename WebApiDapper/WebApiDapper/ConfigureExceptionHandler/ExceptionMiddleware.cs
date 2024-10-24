a6430e9aa1d3ee3bce7583715c7c56b731e460cb
LanhNe <thanhlanhlit@gmail.com>
2024-10-24 23:03:20 +0700

    [api][GlobalHandleException] Add doc & example about Global exception handling

---
+++ WebApiDapper/WebApiDapper/ConfigureExceptionHandler/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using WebApiDapper.ExceptionFilters.ExceptionModels;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WebApiDapper.ConfigureExceptionHandler
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate request)
+        {
+            _next = request;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        public async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsync(new ExceptionModel
+            {
+                Message = "Internal Server Error from the custom middleware",
+                StatusCode = context.Response.StatusCode,
+            }.ToString());
+        }
+    }
+}
